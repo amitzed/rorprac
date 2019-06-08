614a76f742ce7d619aa6dad3ce89cd318dfdf963
Amit <amitzmn@gmail.com>
2019-06-07 22:35:58 -0400

    Added NAME Column to Users TABLE | Included attr to sign_up_params and account_update_params methods inside the RegistrationsController

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_013245) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
