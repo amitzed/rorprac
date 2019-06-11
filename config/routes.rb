a163321ba4bf76c398783a1f249719a0d53370d4
Amit <amitzmn@gmail.com>
2019-06-10 21:00:06 -0400

    Altered Signup page's route from /users/sign_up to /signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
   end
 
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   root to: 'pages#index'
 
 end
