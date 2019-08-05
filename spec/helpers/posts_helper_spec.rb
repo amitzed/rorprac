c5d8f76661a41fa4c23396f7f31c9b6fd55b2ee8
Amit <amitzmn@gmail.com>
2019-08-05 17:49:03 -0400

    Added specs for post_format_partial_path helper method

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -50,4 +50,20 @@ RSpec.describe PostsHelper, :type => :helper do
     end
   end
 
+  context '#post_format_partial_path' do
+    it "return a home_page partial's path" do
+      helper.stub(:current_page?).and_return(true)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/home_page'
+      )
+    end
+
+    it "returns a branch_page partial's path" do
+      helper.stub(:current_page?).and_return(false)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/branch_page'
+      )
+    end
+  end
+
 end
