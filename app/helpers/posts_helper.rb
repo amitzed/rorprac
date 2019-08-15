86fbe7d91349e6ab0596a47af73ff5805818e4d1
Amit <amitzmn@gmail.com>
2019-08-15 17:29:05 -0400

    created infinite scoll.js file to have Facebook-like scroll - and hid the .infinite-scroll elem with CSS - Created _update_paginations.js.erb AND _remove_pagination.js.erb file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -32,4 +32,12 @@ module PostsHelper
     end
   end
 
+  def update_pagination_partial_path
+    if @posts.next_page
+      'posts/posts_pagination_page/update_pagination'
+    else
+      'posts/posts_pagination_page/remove_pagination'
+    end
+  end
+
 end
