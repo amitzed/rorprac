fad3ce99667626696c6e065559420adbfa1479f2
Amit <amitzmn@gmail.com>
2019-08-26 19:36:51 -0400

    Defined several methos in  PostsController - new template for posts created - UI created to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
 
   def hobby
     posts_for_branch(params[:action])
@@ -18,6 +33,11 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
 
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
