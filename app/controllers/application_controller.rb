e1dda73038af231268828111f2760336fe1f8042
Amit <amitzmn@gmail.com>
2019-08-30 16:25:29 -0400

    Validations added to the Post Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+
   def redirect_if_not_signed_in
-    redirect_to root_path !user_signed_in?
+    redirect_to root_path if !user_signed_in?
   end
 
   def redirect_if_signed_in
     redirect_to root_path if user_signed_in?
   end
+
 end
